7ef1255a966f08f55ea4fc8422607dbc7fbe21bf
Gabriele Ciampa <giangabriele.ciampa@gmail.com>
2018-06-14 10:26:43 -0400

    Quatrième commit

--- factorielle.java
+++ factorielle.java
@@ -2,11 +2,13 @@ public class factorielle {
 	public static void main(String[] args) {
 		int factoriel = 1;
 		int valeur;
+		int ajout;
 		valeur = Integer.parseInt(args[0]);
 		while (valeur > 1) {
 			factoriel = factoriel * valeur;
 			valeur = valeur - 1;
 		}
 		System.out.println("La factorielle est : " + factoriel);
+		System.out.println("Le programme est terminé");
 	}
 }
